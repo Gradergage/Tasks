1605a56d9cccd8cc51edc5a630100b08db482b7b
Gradergage <ambulostrator26@gmail.com>
2017-07-04 02:11:41 +0300

    Commentary methods.js

--- ThirdTask/WebContent/js/methods.js
+++ ThirdTask/WebContent/js/methods.js
@@ -2,15 +2,11 @@ $(document).ready(function() {
 	$("#customQueryBttn").click(function()
 			{$("#queryModal").modal('show');
 			});
+	/* Used to link with servlet via ajax and replace Content Table inner HTML*/
 	$(document).on("click", "#executeButton", function() { // When HTML DOM "click" event is invoked on element with ID "somebutton", execute the following function...
 		var params = {query: $("#queryField").val()};
 		$.post("QueryExecutor", $.param(params), function(responseText) {
 			$("#contentTable").html(responseText); 
 		});
     });
-	
-	/*$.get("QueryExecutor", function(responseText) {   // Execute Ajax GET request on URL of "someservlet" and execute the following function with Ajax response text...
-    	$("#contentTable").html(responseText);           // Locate HTML DOM element with ID "somediv" and set its text content with the response text.
-    });
-});*/
 });
